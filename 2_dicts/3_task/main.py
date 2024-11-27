1e5f56191ed5c14d022c7dd48a539e985ae324ff
MametovRustam <144462546+MametovRustam@users.noreply.github.com>
2024-11-27 21:35:33 +0300

    Solu

--- 2_dicts/3_task/main.py
+++ 2_dicts/3_task/main.py
@@ -1,12 +1,17 @@
 def format_phone(phone_number: str) -> str:
-    """Функция возвращает отформатированный телефон.
-
-    Args:
-        phone_number: исходный телефон
-
-    Returns:
-        отформатированный телефон
-    """
-    formatted_phone_number = ""
+    
+cleaned_phone_number = ''.join(filter(str.isdigit, phone_number))
+    if len(cleaned_phone_number) == 10:
+        cleaned_phone_number = '8' + cleaned_phone_number
+    elif len(cleaned_phone_number) == 11 and cleaned_phone_number.startswith('7'):
+        cleaned_phone_number = '8' + cleaned_phone_number[1:]
+    elif len(cleaned_phone_number) == 11 and cleaned_phone_number.startswith('8'):
+        pass
+    else:
+        return cleaned_phone_number
+        
+    formatted_phone_number = f"8 ({cleaned_phone_number[1:4]}) {cleaned_phone_number[4:7]}-{cleaned_phone_number[7:9]}-{cleaned_phone_number[9:11]}"
 
     return formatted_phone_number
+
+
