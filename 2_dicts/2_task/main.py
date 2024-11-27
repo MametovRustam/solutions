0f85353436118b4b1e8e6d5e0dbd3efa9f286045
MametovRustam <144462546+MametovRustam@users.noreply.github.com>
2024-11-27 20:16:23 +0300

    solution - 2.2

--- 2_dicts/2_task/main.py
+++ 2_dicts/2_task/main.py
@@ -1,3 +1,6 @@
+from collections import Counter
+import string 
+
 def top_10_most_common_words(text: str) -> dict[str, int]:
     """Функция возвращает топ 10 слов, встречающихся в тексте.
 
@@ -7,5 +10,17 @@ def top_10_most_common_words(text: str) -> dict[str, int]:
     Returns:
         словарь типа {слово: количество вхождений}
     """
-    most_common = {}
+
+   
+    text = text.lower()
+    
+    translator = str.maketrans('', '', string.punctuation)
+    cleaned_text = text.translate(translator)
+    words = cleaned_text.split()
+    filtered_words = [word for word in words if len(word) >= 3]
+    word_counts = Counter(filtered_words)
+    sorted_word_counts = dict(sorted(word_counts.items(), key=lambda item: (-item[1], item[0])))
+    most_common = dict(list(sorted_word_counts.items())[:10])
+    
     return most_common
+
