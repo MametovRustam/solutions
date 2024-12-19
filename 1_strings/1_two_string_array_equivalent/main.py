feb271724033c80e0e98b44e77f4524770689c98
Honor <mytimeisout@yandex.ru>
2024-12-19 21:43:07 +0300

    1_two_string_array_equivalent_test

--- 1_strings/1_two_string_array_equivalent/main.py
+++ 1_strings/1_two_string_array_equivalent/main.py
@@ -2,3 +2,4 @@ def is_array_string_are_equal(array_string_1: list[str], array_string_2: list[st
     input_string_1 = ''.join(array_string_1).upper()
     input_string_2 = ''.join(array_string_2).upper()
     return input_string_1 == input_string_2
+#solutins_test#
