451079ac4ca2761e2e464e9456b480b2bc52f76f
Mametov Rustam <mytimeisout@yandex.ru>
2024-12-26 17:50:44 +0300

    add solutions

--- 5_iterators/self_batched/self_batched.py
+++ 5_iterators/self_batched/self_batched.py
@@ -1,12 +1,38 @@
-from typing import Generator, Iterable, TypeVar
+from typing import Generator, Iterable, TypeVar, Tuple
 
 T = TypeVar("T")
 
-
-def batched(obj: Iterable[T], n: int) -> Generator[tuple[T], None, None]:
+def batched(iterable: Iterable[T], n: int) -> Generator[Tuple[T, ...], None, None]:
     """Пиши свой код здесь."""
-
+    batch = []
+    for element in iterable:
+        batch.append(element)
+        if len(batch) == n:
+            yield tuple(batch)
+            batch = []
+    if batch:
+        yield tuple(batch)
 
 class Batched:
-    def __init__(self, obj: Iterable[T], n: int):
-        """Реализуй этот класс."""
+    def __init__(self, iterable: Iterable[T], n: int):
+        self.iterable = iterable
+        self.n = n
+        self.batch = []
+        self.iterator = iter(self.iterable)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self) -> Tuple[T, ...]:
+        while len(self.batch) < self.n:
+            try:
+                self.batch.append(next(self.iterator))
+            except StopIteration:
+                break
+
+        if self.batch:
+            result = tuple(self.batch)
+            self.batch = []
+            return result
+
+        raise StopIteration
--- 5_iterators/self_cycle/self_cycle.py
+++ 5_iterators/self_cycle/self_cycle.py
@@ -5,8 +5,24 @@ T = TypeVar("T")
 
 def cycle(obj: Iterable[T]) -> Generator[T, None, None]:
     """Пишите ваш код здесь."""
+    while True:
+        for item in obj:
+            yield item
 
 
 class Cycle:
     def __init__(self, obj: Iterable[T]):
-        """Реализуйте класс"""
+        self.obj = obj
+        self.iterator = iter(obj)
+
+
+    def __iter__(self):
+        return self
+
+
+    def __next__(self) -> T:
+        while True:
+            try:
+                return next(self.iterator)
+            except StopIteration:
+                self.iterator = iter(self.obj)
